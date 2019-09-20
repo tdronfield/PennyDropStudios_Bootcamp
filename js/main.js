3f12f52f105167d22d6b9df3f6e1c1d82cdaec3f
Mikka Azores <mikkaazores@Mikkas-MacBook-Pro-2.local>
2019-09-20 16:28:36 -0400

    Video Javascript

--- js/main.js
+++ js/main.js
@@ -13,7 +13,13 @@ import Team from './modules/DataObject.js';
 	const memberImage = document.querySelector('.u-image'),
 		memberName = document.querySelector('.u-name'),
 		memberRole = document.querySelector('.u-role'),
-		memberDesc = document.querySelector('.u-bio');
+		memberDesc = document.querySelector('.u-bio'),
+		houseVideo = document.querySelector('.about-video'),
+		playButton = document.querySelector('#play'),
+		pauseButton = document.querySelector('#pause'),
+		stopButton = document.querySelector('#stop'),
+		volDownButton = document.querySelector('#voldown'),
+		volUpButton = document.querySelector('#volup');
 
 	const bioData = [ // houseData[0][1]
 	[`Mikka Azores`, `UI/UX, WebDev`, `Mikka is a 22-year old Multimedia Designer who moved all the way from sunny Philippines to Canada eight months ago in order to further her skills and knowledge in design.
@@ -50,7 +56,38 @@ import Team from './modules/DataObject.js';
 		bio.classList.toggle("slideToggle");
 	}
 
+	//pause video on click
+	function pauseVideo() {
+		houseVideo.pause();
+	}
+
+	function playVideo() {
+		houseVideo.play();
+	}
+
+	function stopVideo() {
+		houseVideo.pause();
+		houseVideo.currentTime = 0;
+		//console.log(forwardedTime);
+	}
+
+	function volumeUp() {
+		let houseVideoVolUp = houseVideo.volume + 0.5;
+
+		houseVideo.volume = houseVideoVolUp;
+	}
+
+	function volumeDown() {
+		let houseVideoVolDown = houseVideo.volume - 0.5;
+
+		houseVideo.volume = houseVideoVolDown;
+	}
 
 	button.addEventListener("click", hamburgerMenu, false);
 	bioExit.addEventListener("click", hideBio);
+	playButton.addEventListener("click", playVideo);
+	pauseButton.addEventListener("click", pauseVideo);
+	stopButton.addEventListener("click", stopVideo);
+	volUpButton.addEventListener("click", volumeUp);
+	volDownButton.addEventListener("click", volumeDown);
 })();
